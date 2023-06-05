a2fefce81f417365e78ea483d91c7fa362d857b2
akashopp <106897340+akashopp@users.noreply.github.com>
2023-06-05 23:00:54 +0530

    Create LongComSubseq.cpp

---
+++ LongComSubseq.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+int f(int i, int j, string &s, string &t, int &n, int &m, vector<vector<int>>& dp){
+	if(i == n || j == m) return 0; 
+	if(dp[i][j] != -1) return dp[i][j];
+	if(s[i] == t[j]) return dp[i][j] = 1 + f(i+1, j+1, s, t, n, m, dp);
+	return dp[i][j] = max(f(i, j+1, s, t, n, m, dp), f(i+1, j, s, t, n, m, dp));
+}
+int lcs(string s, string t)
+{
+	//Write your code here
+	int n = s.size(), m = t.size(); 
+	vector<vector<int>>dp(n, vector<int>(m, -1)); 
+	return f(0, 0, s, t, n, m, dp); 
+}
